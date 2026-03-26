de8fdbdcac175806cb224a8eb6ff01a71b8b4a0e
sanat-sg7 <2022csb1119@iitrpr.ac.in>
2026-03-27 02:57:27 +0530

    hi

--- train.py
+++ train.py
@@ -21,4 +21,4 @@ model.fit(X_train, y_train)
 with open("model.pkl", "wb") as f:
     pickle.dump(model, f)
 
-print("Model trained and saved as model.pkl")
+print("Model trained and saved as model.pkl, okay?")
