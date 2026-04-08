d599be0251826344c65025efd55b0fc632d07623
sanat-sg7 <2022csb1119@iitrpr.ac.in>
2026-04-08 14:17:07 +0530

    added a comment

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ y = iris.target
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
-
+# adding a comment
 # Train model
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
